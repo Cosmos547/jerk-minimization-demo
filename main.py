c9438257817f04c9709cdf7925d8a40ec3ad05ac
Wenhan Zhu (Cosmos) <cosmos.zhu@uwaterloo.ca>
2018-05-02 15:25:03 -0400

    fixed bug and updated graph

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ config = pyglet.gl.Config(sample_buffers=1, samples=4)
 window = pyglet.window.Window(width=640, height=640, config=config)
 p1 = Point(40.0, 40.0)
 p1.v = 200
+p1.a = 30
 p2 = Point(600.0, 600.0)
 p2.v = 400
+p2.a = 50
 path = Path([p1, p2])
 agent = Agent('car_agent.png')
 agent.setAtri(path.plist)
 
 jmagent = JMAgent('car_agent.png')
 jmagent.setAtri(path.plist)
-print(jmagent.poly_a)
+# print(jmagent.poly_a)
 
 jmagent.make_plot()
 
